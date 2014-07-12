d4f74d8d8045f9e76eee39dfadf57f4012724761
Manu Goyal <manu.goyal2013@gmail.com>
2014-07-12 10:01:57 -0700

    Removed the elpa directory from the cache

--- elpa/magit-tramp-20140517.753/magit-tramp-pkg.el
+++
@@ -1,3 +0,0 @@
-(define-package "magit-tramp" "20140517.753" "git method for TRAMP"
-  '((magit "1.2.0"))
-  :url "https://github.com/sigma/magit-tramp")
--- elpa/multi-term-20140620.1854/multi-term-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "multi-term" "20140620.1854" "Managing multiple terminal buffers in Emacs." (quote nil))

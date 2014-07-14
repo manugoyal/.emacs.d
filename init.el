8a171ae1f4026ecc6773930679d4dd5226357136
Manu Goyal <manugoyal@google.com>
2014-07-14 13:40:10 -0700

    A few small changes
    1. Actually installed auctex, so it loads on startup
    
    2. Rather than making variable-width font the default and turning it off
    in prog-mode, we explicitly turn it on in text-mode
    
    3. Enable flyspell for text and programming buffers

--- init.el
+++ init.el
@@ -25,8 +25,8 @@
 ;; any package that isn't installed, we install it. To obtain this list on a
 ;; full installation, get the value of the 'package-activated-list variable.
 (defconst installed-packages
-  '(flx-ido monokai-theme flx ace-jump-mode ag async autopair company dired+ dired-sort-menu emacs-eclim exec-path-from-shell find-file-in-project flycheck f framemove fuzzy go-autocomplete go-mode haskell-mode highlight-indentation idomenu iedit jedi auto-complete epc ctable concurrent js2-mode less-css-mode magit-push-remote magit-tramp magit git-rebase-mode git-commit-mode markdown-mode multi-term multiple-cursors org pkg-info epl popup python-environment deferred pyvenv request s scss-mode smex sml-mode solarized-theme sql-indent tuareg caml undo-tree virtualenv web-mode websocket wgrep-ag wgrep wrap-region dash yasnippet)
-  "A list of packages that should be installed at startup"
+  '(auctex ace-jump-mode ag async autopair company dired+ dired-sort-menu emacs-eclim exec-path-from-shell f find-file-in-project flx-ido flx flycheck framemove fuzzy go-autocomplete go-mode haskell-mode highlight-indentation idomenu iedit jedi auto-complete epc ctable concurrent js2-mode less-css-mode leuven-theme magit-push-remote magit-tramp magit git-rebase-mode git-commit-mode markdown-mode monokai-theme multi-term multiple-cursors org pkg-info epl popup python-environment deferred pyvenv request s scss-mode smex sml-mode solarized-theme sql-indent tuareg caml undo-tree virtualenv web-mode websocket wgrep-ag wgrep wrap-region dash yasnippet zenburn-theme)
+  "A list of packages that should be installed at start-up."
   )
 (dolist (package installed-packages)
   (if (not (package-installed-p package))
@@ -49,4 +49,4 @@
 (org-babel-load-file configuration-file)
 
 (provide 'init)
-;; init.el ends here
+;;; init.el ends here

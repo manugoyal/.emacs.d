537383d84134f7fecae5e0d9d25736b9526ad0e7
Manu Goyal <manu.goyal2013@gmail.com>
2015-01-14 15:12:25 -0500

    Overhauled package loading
    Started using use-package, since it cleanly organizes everything, handles
    installing packages that aren't on the system, and lets us load packages
    only when needed. This removes the need for maintaining the list in
    init.el of installed packages.

--- init.el
+++ init.el
@@ -11,36 +11,21 @@
 (defconst downloads-dir (concat base-dir "downloads/"))
 (defconst configuration-file (concat base-dir "README.org"))
 
-;; Loads the cl library, since that's important and should come before
-;; everything else
-(require 'cl)
-
-;; We keep a list of installed packages here. We go through this list, and for
-;; any package that isn't installed, we install it. To obtain this list on a
-;; full installation, get the value of the 'package-activated-list variable.
-(defconst installed-packages
-  '(wrap-region dash ace-jump-mode ag s auctex cider queue pkg-info epl
-                clojure-mode company-anaconda anaconda-mode f json-rpc company
-                company-c-headers company-go dired+ dired-sort-menu epc ctable
-                concurrent deferred exec-path-from-shell find-file-in-project
-                flycheck framemove fuzzy go-mode haskell-mode helm async
-                highlight-indentation iedit js2-mode less-css-mode
-                magit-push-remote magit git-rebase-mode git-commit-mode
-                magit-tramp markdown-mode multiple-cursors paredit popup
-                python-environment pyvenv rainbow-mode request scss-mode
-                sml-mode sql-indent tuareg caml undo-tree web-mode websocket
-                wgrep-ag wgrep yasnippet zenburn-theme)
-  )
-
 ;; Sets up package
 (require 'package)
 (setq package-archives
       '(("gnu" . "http://elpa.gnu.org/packages/")
         ("melpa" . "http://melpa.milkbox.net/packages/")))
-(package-initialize)
+;; Initialize the package system but don't load all the packages
+(package-initialize nil)
+(setq package-enable-at-startup nil)
+;; Refresh the package list if it isn't there
 (unless package-archive-contents (package-refresh-contents))
-(dolist (package installed-packages)
-  (unless (package-installed-p package) (package-install package)))
+
+;; We use use-package to organize our package loading and initialization, so
+;; load that here
+(unless (package-installed-p 'use-package) (package-install 'use-package))
+(require 'use-package)
 
 ;; Loads custom.el. The custom.el file should only contain settings that are
 ;; long lists of things or contain data too long or unwieldy to type out
@@ -53,7 +38,7 @@
 (require 'org-id)
 (require 'org-element)
 (require 'org)
+;; Load our configuration file
 (org-babel-load-file configuration-file)
 
 (provide 'init)
-;;; init.el ends here
